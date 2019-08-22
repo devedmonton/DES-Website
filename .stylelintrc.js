5bff50b1a7085af0b1163a7b6aca44164f947109
Jeff <rustyangel@gmail.com>
2019-08-22 15:36:07 -0600

    Stop the auto-formatters from fighting

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,5 +2,6 @@ module.exports = {
     extends: ['@zazen/stylelint-config'],
     rules: {
         'at-rule-no-unknown': [true, { ignoreAtRules: ['include', 'mixin'] }],
+        'value-list-comma-newline-after': null,
     },
 }
