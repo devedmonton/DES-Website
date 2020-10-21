51789f94a8ad8ed95f8d923ea0752d6bebf5d161
Derryk Boyd <derrykboyd@gmail.com>
2020-10-20 22:32:01 -0600

    Fix alternating background color

--- pages/index.vue
+++ pages/index.vue
@@ -56,7 +56,7 @@
       class="relative bg-grey-000 pt-24 -mt-8 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8"
     >
       <div class="absolute inset-0">
-        <div class="bg-white h-1/3 sm:h-2/3"></div>
+        <div class="h-1/3 sm:h-2/3"></div>
       </div>
       <div class="relative max-w-7xl mx-auto">
         <div class="text-center">
@@ -164,7 +164,7 @@
       class="relative bg-white pt-24 -mt-8 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8"
     >
       <div class="absolute inset-0">
-        <div class="bg-white h-1/3 sm:h-2/3"></div>
+        <div class="h-1/3 sm:h-2/3"></div>
       </div>
       <div class="relative max-w-7xl mx-auto">
         <div class="text-center">
@@ -347,7 +347,7 @@
       class="relative bg-grey-000 pt-24 -mt-8 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8"
     >
       <div class="absolute inset-0">
-        <div class="bg-white h-1/3 sm:h-2/3"></div>
+        <div class="h-1/3 sm:h-2/3"></div>
       </div>
       <div class="relative max-w-7xl mx-auto">
         <div class="text-center">
