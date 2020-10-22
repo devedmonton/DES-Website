8b0f80cc1c5e5980f391ddedf6da2c89ff08aa3d
Mark Bennett <mark@burmis.ca>
2020-10-22 04:20:10 +0000

    Add get involved developer image
    https://unsplash.com/photos/YK0HPwWDJ1I?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink

--- pages/index.vue
+++ pages/index.vue
@@ -177,7 +177,7 @@
             <div class="flex-shrink-0">
               <img
                 class="h-48 w-full object-cover"
-                src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
+                src="~/assets/homepage/get-involved-dev.jpg"
                 alt=""
               />
             </div>
