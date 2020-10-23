702f142b7ae1ab4713d836ddf9f9c66757dbdee1
Derryk Boyd <derrykboyd@gmail.com>
2020-10-22 19:46:45 -0600

    Fix wide row top

--- pages/index.vue
+++ pages/index.vue
@@ -6,43 +6,42 @@
       link="/contact"
       buttonText="Contact Us"
     />
-    <div id="about">
-      <div class="py-8 bg-white overflow-hidden">
-        <div
-          class="relative mx-8 sm:mx-12 pb-8 lg:my-4 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center"
-        >
-          <div class="relative">
-            <h3
-              class="text-3xl leading-8 font-extrabold text-gray-900 tracking-tight sm:text-3xl sm:leading-9"
-            >
-              Dev Edmonton Society
-            </h3>
-            <h4
-              class="text-lg leading-8 font-bold text-gray-900 tracking-tight sm:text-lg sm:leading-9"
-            >
-              Fostering a stronger software development community in Edmonton.
-            </h4>
-            <p class="mt-3 text-lg leading-7 text-gray-500">
-              We are made up of people who believe that an impactful, diverse,
-              and collaborative developer community enriches us all. We realize
-              this vision by sharing knowledge and ideas, understanding the
-              community and its needs, promoting events and initiatives, and
-              supporting technology meetups and organizations.
-            </p>
-          </div>
 
-          <div class="mt-10 -mx-4 relative lg:mt-0">
-            <div class="slack-content relative p-4 text-center">
-              <p class="text-xl text-brand-secondary">
-                <a href="https://devedmonton-invite.herokuapp.com/">
-                  Join our community on Slack
-                </a>
-              </p>
-              <img class="max-w-md m-auto" src="~assets/logos/Slack_RGB.svg" />
+    <div id="about" class="py-8 bg-white overflow-hidden max-w-7xl mx-auto sm:px-4">
+      <div
+        class="relative mx-8 sm:mx-12 pb-8 lg:my-4 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center"
+      >
+        <div class="relative">
+          <h3
+            class="text-3xl leading-8 font-extrabold text-gray-900 tracking-tight sm:text-3xl sm:leading-9"
+          >
+            Dev Edmonton Society
+          </h3>
+          <h4
+            class="text-lg leading-8 font-bold text-gray-900 tracking-tight sm:text-lg sm:leading-9"
+          >
+            Fostering a stronger software development community in Edmonton.
+          </h4>
+          <p class="mt-3 text-lg leading-7 text-gray-500">
+            We are made up of people who believe that an impactful, diverse, and
+            collaborative developer community enriches us all. We realize this
+            vision by sharing knowledge and ideas, understanding the community
+            and its needs, promoting events and initiatives, and supporting
+            technology meetups and organizations.
+          </p>
+        </div>
+
+        <div class="mt-10 -mx-4 relative lg:mt-0">
+          <div class="slack-content relative p-4 text-center">
+            <p class="text-xl text-brand-secondary">
               <a href="https://devedmonton-invite.herokuapp.com/">
-                <v-button appearance="inverted">Join Slack</v-button>
+                Join our community on Slack
               </a>
-            </div>
+            </p>
+            <img class="max-w-md m-auto" src="~assets/logos/Slack_RGB.svg" />
+            <a href="https://devedmonton-invite.herokuapp.com/">
+              <v-button appearance="inverted">Join Slack</v-button>
+            </a>
           </div>
         </div>
       </div>
