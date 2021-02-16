c3e70681ca519a5d37ffcdc73d217836836fa0f4
Derryk Boyd <derrykboyd@gmail.com>
2021-02-16 00:24:38 -0700

    Expand try block for function

--- netlify/functions/newsletter-signup.js
+++ netlify/functions/newsletter-signup.js
@@ -1,14 +1,14 @@
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 // eslint-disable-next-line no-unused-vars
 exports.handler = async function (event, context) {
-    const apiKey = process.env.MAILCHIMP_API_KEY;
-    const listID = process.env.MAILCHIMP_LIST_ID;
-    mailchimp.setConfig({
-        apiKey,
-        server: "us1",
-    });
-    const email = JSON.parse(event.body).email;
     try {
+        const apiKey = process.env.MAILCHIMP_API_KEY;
+        const listID = process.env.MAILCHIMP_LIST_ID;
+        mailchimp.setConfig({
+            apiKey,
+            server: "us1",
+        });
+        const email = JSON.parse(event.body).email;
         const res = await mailchimp.lists.addListMember(listID, {
             email_address: email,
             status: "pending",
