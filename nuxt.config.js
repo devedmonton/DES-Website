fdef04f59da9e984126b6d78a7d76645aaa64346
Derryk Boyd <derrykboyd@gmail.com>
2021-02-15 19:46:36 -0700

    Fix formatting in .js files

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,66 +1,66 @@
 export default {
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: 'Dev Edmonton Society',
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
-  /*
-   ** Auto Import Components
-   */
-  components: true,
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
-  /*
-   ** Nuxt.js build-modules
-   */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
-    // Doc: https://google-analytics.nuxtjs.org
-    '@nuxtjs/google-analytics',
-    // Doc https://pwa.nuxtjs.org/
-    '@nuxtjs/pwa',
-  ],
-  /*
-   ** PWA Settings
-   */
-  pwa: {
-    meta: { mobileApp: false },
-  },
-  /*
-   ** Build configuration
-   */
-  build: {
     /*
-     ** You can extend webpack config here
+     ** Headers of the page
      */
-    extend(config, ctx) { },
-  },
-  googleAnalytics: {
-    id: 'UA-180778341-1'
-  },
-}
+    head: {
+        title: "Dev Edmonton Society",
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+            {
+                hid: "description",
+                name: "description",
+                content: process.env.npm_package_description || "",
+            },
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+    /*
+     ** Auto Import Components
+     */
+    components: true,
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: "#fff" },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js build-modules
+     */
+    buildModules: [
+        // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+        "@nuxtjs/tailwindcss",
+        // Doc: https://google-analytics.nuxtjs.org
+        "@nuxtjs/google-analytics",
+        // Doc https://pwa.nuxtjs.org/
+        "@nuxtjs/pwa",
+    ],
+    /*
+     ** PWA Settings
+     */
+    pwa: {
+        meta: { mobileApp: false },
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {},
+    },
+    googleAnalytics: {
+        id: "UA-180778341-1",
+    },
+};
--- test/ava.setup.js
+++ test/ava.setup.js
@@ -1,16 +1,14 @@
-require('browser-env')()
-const hooks = require('require-extension-hooks')
-const Vue = require('vue')
+require("browser-env")();
+const hooks = require("require-extension-hooks");
+const Vue = require("vue");
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // https://github.com/nuxt/create-nuxt-app/issues/180#issuecomment-463069941
-window.Date = global.Date = Date
+window.Date = global.Date = Date;
 
-hooks('vue')
-    .plugin('vue')
-    .push()
-hooks(['vue', 'js'])
+hooks("vue").plugin("vue").push();
+hooks(["vue", "js"])
     .exclude(({ filename }) => filename.match(/\/node_modules\//))
-    .plugin('babel')
-    .push()
+    .plugin("babel")
+    .push();
