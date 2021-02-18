11a97c53b0906cbcb6499fadeb4269bb6196a4df
Derryk Boyd <derrykboyd@gmail.com>
2021-02-17 22:50:26 -0700

    Add a sponsors page

---
+++ pages/sponsors.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 divide-y">
+        <!-- This example requires Tailwind CSS v2.0+ -->
+        <h1 class="text-2xl font-bold py-4">Sponsors</h1>
+
+        <div class="sm:flex flex-col py-8">
+            <div class="mb-4 pb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
+                <a href="https://tailwindcss.com">
+                    <img
+                        class="max-w-xs"
+                        src="~/assets/logos/tailwindcss-logo.svg"
+                        alt="tailwindcss"
+                    />
+                </a>
+            </div>
+            <div>
+                <h4 class="text-2xl font-bold">Tailwind CSS</h4>
+                <p class="mt-1">
+                    Thanks to
+                    <a
+                        class="text-brand-primary underline"
+                        href="https://tailwindcss.com"
+                        >Tailwind CSS</a
+                    >
+                    for providing a team account for
+                    <a
+                        class="text-brand-primary underline"
+                        href="https://tailwindui.com"
+                        >Tailwind UI</a
+                    >
+                    which we used to help develop this site.
+                </p>
+            </div>
+        </div>
+        <div class="sm:flex flex-col py-8">
+            <div class="mb-4 pb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
+                <a href="https://1password.com/">
+                    <img
+                        class="max-w-xs"
+                        src="~/assets/logos/1P-logo-word.png"
+                        alt="1 Password"
+                    />
+                </a>
+            </div>
+            <div>
+                <h4 class="text-2xl font-bold">1Password</h4>
+                <p class="mt-1">
+                    Thanks to
+                    <a
+                        class="text-brand-primary underline"
+                        href="https://1password.com/"
+                        >1Password</a
+                    >
+                    for providing a team account for DES members to manage our
+                    passwords.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
