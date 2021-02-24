88870cabba853ecbffc8089c502847c5b6b27e16
Joshua MacSween <joshmacsween@gmail.com>
2021-02-24 10:00:15 -0700

    Added Lauren's info, added linkedIn icon

--- components/BoardMember.vue
+++ components/BoardMember.vue
@@ -14,6 +14,26 @@
                     <div class="text-lg font-medium">
                         <h3>{{ name }}</h3>
                         <p class="text-indigo-600">{{ title }}</p>
+                        <div v-if="linkedInAccount" class="flex pb-1">
+                            <span class="sr-only">linkedIn</span>
+                            <a :href="linkedInAccount">
+                                <svg
+                                    class="inline w-5 h-5"
+                                    fill="#c0c0c0"
+                                    viewBox="0 0 24 24"
+                                    aria-hidden="true"
+                                    preserveAspectRatio="xMidYMid meet"
+                                >
+                                    <path
+                                        d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"
+                                        clip-rule="evenodd"
+                                    />
+                                </svg>
+                                <span class="text-xs pt-0.5 text-gray-500"
+                                    ><i> {{ name }} </i></span
+                                >
+                            </a>
+                        </div>
                         <div v-if="twitterHandle" class="flex pb-1">
                             <span class="sr-only">Twitter</span>
                             <!-- using v-bind directive shorthand to construct twitter link -->
@@ -72,7 +92,7 @@ export default {
         description: { type: String, default: "" },
         imageLink: { type: String, default: "" },
         twitterHandle: { type: String, default: "" },
-        twitterLink: { type: String, default: "" },
+        linkedInAccount: { type: String, default: "" },
         slackAccount: { type: String, default: "" },
     },
 };
--- pages/board.vue
+++ pages/board.vue
@@ -53,6 +53,7 @@
                                 :description="member.description"
                                 :image-link="member.imageLink"
                                 :twitter-handle="member.twitterHandle"
+                                :linked-in-account="member.linkedInAccount"
                                 :slack-account="member.slackAccount"
                             />
                         </ul>
@@ -81,6 +82,7 @@ export default {
                     description:
                         "When starting with Ruby on Rails in 2009, Mark joined an amazing meetup in Sydney, Australia. Once home, he saw the value of a meetup community to developers, and founded YEGrb, Edmonton’s Ruby Meetup. Over the years he has helped with Exchange.js, BarCamp, and others. These experiences led him to establish DES in 2016, so developers could have a place where they could succeed and grow together. <br /><br />Mark is passionate about the developer community, and its role in Edmonton. His hope is to see more developers succeeding in bigger challenges in the future.<br /><br /> When not busy with DES he’s herding bits with <a class='text-indigo-600' href='https://burmis.ca/'>Burmis Studio</a>, chasing after his kids, or out for a run. Say hi on Slack or Twitter!",
                     twitterHandle: "MarkBennett",
+                    linkedInAccount: "",
                     slackAccount: "Mark",
                 },
                 {
@@ -91,16 +93,19 @@ export default {
                     description:
                         "Ian Phillipchuk is a Team Lead and Senior Developer at <a class='text-indigo-600' href='https://punchcard.io/'>Punchcard</a>. Professionally, he does Software Consulting, Mobile and Web Development, as well as managing software teams and client projects.<br /><br /> At Dev Edmonton, he wants to grow the society to do more to help all the Devs in Edmonton, from providing resources for Meetups and Conferences for veteran developers, and helping new devs find their way and their first opportunities.<br /><br />Personally, he's probably getting fragged in Destiny 2 PVP for the bajillionth time, or building Lego with his kids.",
                     twitterHandle: "Protagorist",
+                    linkedInAccount: "",
                     slackAccount: "Ian Phillipchuck",
                 },
                 {
                     id: 3,
                     name: "Lauren Briske",
                     title: "Treasurer",
-                    imageLink: require("~/assets/boardPictures/alyssaDiehl.jpg"),
+                    imageLink: require("~/assets/boardPictures/laurenBriske.jpg"),
                     description:
-                        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem error praesentium repudiandae labore dolores veritatis deleniti laudantium eos, nemo debitis, odit voluptates rerum sapiente quidem? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem error praesentium repudiandae labore dolores veritatis deleniti laudantium eos, nemo debitis, odit voluptates rerum sapiente quidem?",
+                        "Lauren Briske is the Interim Director at <a class='text-indigo-600' href='https://startupedmonton.com/'>Startup Edmonton</a>. She is a passionate supporter of the startup and tech community and knows that a thriving tech ecosystem grows on the shoulders of a strong community.<br /><br />Through the Dev Edmonton Society, her goal is to empower local leaders to contribute to a growing community and to welcome more students, experienced developers, and aspiring developers into this great community.<br /><br />When she’s not working or volunteering, she’s probably riding down a mountain somewhere on skis or bikes.",
                     twitterHandle: "",
+                    linkedInAccount:
+                        "https://www.linkedin.com/in/lauren-briske-03ba902b/",
                     slackAccount: "laurenbriske",
                 },
                 {
@@ -111,6 +116,7 @@ export default {
                     description:
                         "Mandy Meindersma is a Backend Developer at <a class='text-indigo-600' href='https://dotdash.com/'>Dotdash</a>. She loves web development, teaching kidlets about computing science and hardware (making robots, light up costumes and IoT type of things).<br /><br /> She has loved getting more involved with DES through YEGWIT (Edmonton Women In Tech), CODEVID-19 (COVID-19 Hackathon) and now being able to help with organization and memberships!<br /><br /> Outside of tech she absolutely loves acro yoga, her cat Goose, and writing letters.",
                     twitterHandle: "",
+                    linkedInAccount: "",
                     slackAccount: "Mandy Meindersma",
                 },
                 {
@@ -121,6 +127,7 @@ export default {
                     description:
                         "Abram Hindle is an associate professor of Computing Science at the University of Alberta. His research focuses on problems relating to mining software repositories, improving software engineering-oriented information retrieval with contextual information, the impact of software maintenance on software energy consumption, and how software engineering informs computer music.<br /><br /> He likes applying machine learning in music, art, and science. Sadly Abram has no taste in music and produces reprehensible sounding noise using his software development abilities.<br /><br /> Abram received a PhD in computer science from the University of Waterloo, and Masters and Bachelors in Computer Science from the University of Victoria. Contact him at abram.hindle@ualberta.ca <br /><a class='text-indigo-600' href='https://softwareprocess.es'>softwareprocess.es</a>",
                     twitterHandle: "",
+                    linkedInAccount: "",
                     slackAccount: "Abram",
                 },
                 {
@@ -129,8 +136,9 @@ export default {
                     title: "Member at large",
                     imageLink: require("~/assets/boardPictures/alyssaDiehl.jpg"),
                     description:
-                        "Alyssa has been working in the field of Career Development, Recruitment & HR for +7 years. She is interested in the tech & innovation industry here in Alberta and seeing a diverse labour market & workforce.<br /><br /> Her passion lies in assisting job seekers find their future career path and loves to connect with job seekers from any field and experience level.<br /><br />  Alyssa is always available to give assistance on market insights, resume review or LinkedIn tips! ",
+                        "Alyssa has been working in the field of Career Development, Recruitment & HR for +7 years. She is interested in the tech & innovation industry here in Alberta and seeing a diverse labour market & workforce.<br /><br /> Her passion lies in assisting job seekers find their future career path and loves to connect with job seekers from any field and experience level.<br /><br />  Alyssa is always available to give assistance on market insights, resume review or LinkedIn tips! <br /><a class='text-indigo-600' href='https://manpowerab.com/development/'>Manpower</a>",
                     twitterHandle: "Lyss_diehl",
+                    linkedInAccount: "https://ca.linkedin.com/in/alyssajdiehl",
                     slackAccount: "Alyssa Diehl",
                 },
                 {
@@ -139,8 +147,10 @@ export default {
                     title: "Member at large",
                     imageLink: require("~/assets/boardPictures/celiaNicholls.jpg"),
                     description:
-                        "Celia is in her final semester of Web Design and Development in the Digital Media and IT program at NAIT (although she sometimes feels as though she should have gone into pest control, she spends so much time debugging).<br /><br /> Her strengths include UX/UI and responsive design, front-end development and content creation. She is passionate about helping to increase student engagement with Dev Edmonton as a way to pay forward all the mentorship and help she’s received from Devs in the community.<br /><br /> In her free time, she enjoys writing and drawing comics and dabbling in esoteric crafts like needlepoint and macramé.",
+                        "Celia is in her final semester of Web Design and Development in the Digital Media and IT program at NAIT (although she sometimes feels as though she should have gone into pest control, she spends so much time debugging).<br /><br /> Her strengths include UX/UI and responsive design, front-end development and content creation. She is passionate about helping to increase student engagement with Dev Edmonton as a way to pay forward all the mentorship and help she’s received from devs in the community.<br /><br /> In her free time, she enjoys writing and drawing comics and dabbling in esoteric crafts like needlepoint and macramé.",
                     twitterHandle: "",
+                    linkedInAccount:
+                        "https://www.linkedin.com/in/celia-nicholls/",
                     slackAccount: "Celia Nicholls",
                 },
             ],
