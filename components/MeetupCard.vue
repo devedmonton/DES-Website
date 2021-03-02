904ab2b3823abbbeae72615f8e0a87597b9dde5c
Derryk Boyd <derrykboyd@gmail.com>
2021-03-01 22:17:50 -0700

    Add a card for each meetup

--- components/MeetupCard.vue
+++ components/MeetupCard.vue
@@ -4,18 +4,21 @@
     >
         <div>
             <img
-                class="w-10 h-10 rounded-sm inline-flex"
+                :class="`w-12 h-12 rounded-md inline-flex p-1 ${
+                    logoBg ? logoBg : null
+                }`"
                 :src="logo"
                 :alt="`${name}-logo`"
             />
         </div>
         <div class="mt-4">
             <h3 class="text-lg font-medium">
-                <a :href="linkTo" class="focus:outline-none">
+                <a v-if="linkTo" :href="linkTo" class="focus:outline-none">
                     <!-- Extend touch target to entire panel -->
                     <span class="absolute inset-0" aria-hidden="true"></span>
                     {{ name }} - {{ slack }}
                 </a>
+                <p v-else>{{ name }} - {{ slack }}</p>
             </h3>
             <p class="mt-2 text-sm text-gray-500">
                 Doloribus dolores nostrum quia qui natus officia quod et
@@ -24,6 +27,7 @@
             </p>
         </div>
         <span
+            v-if="linkTo"
             class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400"
             aria-hidden="true"
         >
@@ -45,6 +49,7 @@
 export default {
     props: {
         logo: { type: String, required: true },
+        logoBg: { type: String, default: "" },
         name: { type: String, required: true },
         linkTo: { type: String, required: true },
         slack: { type: String, required: true },
--- pages/index.vue
+++ pages/index.vue
@@ -402,124 +402,38 @@
                         :link-to="meetups.javascript.linkTo"
                         :slack="meetups.javascript.slack"
                     />
-
-                    <div
-                        class="sm:rounded-tr-lg relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500"
-                    >
-                        <div>
-                            <span
-                                class="rounded-lg inline-flex p-3 bg-purple-100 text-purple-700 ring-4 ring-white"
-                            >
-                                <!-- Heroicon name: outline/badge-check -->
-                                <svg
-                                    class="h-6 w-6"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    aria-hidden="true"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
-                                    />
-                                </svg>
-                            </span>
-                        </div>
-                        <div class="mt-8">
-                            <h3 class="text-lg font-medium">
-                                <a href="#" class="focus:outline-none">
-                                    <!-- Extend touch target to entire panel -->
-                                    <span
-                                        class="absolute inset-0"
-                                        aria-hidden="true"
-                                    ></span>
-                                    Benefits
-                                </a>
-                            </h3>
-                            <p class="mt-2 text-sm text-gray-500">
-                                Doloribus dolores nostrum quia qui natus officia
-                                quod et dolorem. Sit repellendus qui ut at
-                                blanditiis et quo et molestiae.
-                            </p>
-                        </div>
-                        <span
-                            class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400"
-                            aria-hidden="true"
-                        >
-                            <svg
-                                class="h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z"
-                                />
-                            </svg>
-                        </span>
-                    </div>
-
-                    <div
-                        class="relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500"
-                    >
-                        <div>
-                            <span
-                                class="rounded-lg inline-flex p-3 bg-light-blue-100 text-light-blue-700 ring-4 ring-white"
-                            >
-                                <!-- Heroicon name: outline/users -->
-                                <svg
-                                    class="h-6 w-6"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    aria-hidden="true"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-                                    />
-                                </svg>
-                            </span>
-                        </div>
-                        <div class="mt-8">
-                            <h3 class="text-lg font-medium">
-                                <a href="#" class="focus:outline-none">
-                                    <!-- Extend touch target to entire panel -->
-                                    <span
-                                        class="absolute inset-0"
-                                        aria-hidden="true"
-                                    ></span>
-                                    Schedule a one-on-one
-                                </a>
-                            </h3>
-                            <p class="mt-2 text-sm text-gray-500">
-                                Doloribus dolores nostrum quia qui natus officia
-                                quod et dolorem. Sit repellendus qui ut at
-                                blanditiis et quo et molestiae.
-                            </p>
-                        </div>
-                        <span
-                            class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400"
-                            aria-hidden="true"
-                        >
-                            <svg
-                                class="h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z"
-                                />
-                            </svg>
-                        </span>
-                    </div>
+                    <MeetupCard
+                        :name="meetups.mobileDev.name"
+                        :logo="meetups.mobileDev.logo"
+                        :link-to="meetups.mobileDev.linkTo"
+                        :slack="meetups.mobileDev.slack"
+                        logo-bg="bg-blue-100"
+                    />
+                    <MeetupCard
+                        :name="meetups.edmontonPy.name"
+                        :logo="meetups.edmontonPy.logo"
+                        :link-to="meetups.edmontonPy.linkTo"
+                        :slack="meetups.edmontonPy.slack"
+                    />
+                    <MeetupCard
+                        :name="meetups.go.name"
+                        :logo="meetups.go.logo"
+                        :link-to="meetups.go.linkTo"
+                        :slack="meetups.go.slack"
+                    />
+                    <MeetupCard
+                        :name="meetups.dotNet.name"
+                        :logo="meetups.dotNet.logo"
+                        :link-to="meetups.dotNet.linkTo"
+                        :slack="meetups.dotNet.slack"
+                    />
+                    <MeetupCard
+                        :name="meetups.devOps.name"
+                        :logo="meetups.devOps.logo"
+                        :link-to="meetups.devOps.linkTo"
+                        :slack="meetups.devOps.slack"
+                        logo-bg="bg-blue-100"
+                    />
                 </div>
             </div>
         </div>
@@ -543,10 +457,41 @@ export default {
                     slack: "#meetup-ruby",
                 },
                 javascript: {
-                    name: "exchange.js",
+                    name: "Exchange.js",
                     logo: require("../assets/logos/JavaScript-logo-240px.png"),
                     linkTo: "https://exchangejs.com/",
                     slack: "#meetup-javascript",
+                },
+                mobileDev: {
+                    name: "Mobile Dev Meetup",
+                    logo: require("../assets/logos/mobile-dev.svg"),
+                    linkTo: "",
+                    slack: "#meetup-mobile-dev",
+                },
+                edmontonPy: {
+                    name: "Edmonton.Py",
+                    logo: require("../assets/logos/python-logo.svg"),
+                    linkTo: "",
+                    slack: "#meetup-edmontonpy",
+                },
+                go: {
+                    name: "Go Meetup",
+                    logo: require("../assets/logos/Go-Logo_Blue.svg"),
+                    linkTo: "",
+                    slack: "#meetup-go",
+                },
+                dotNet: {
+                    name: "Edmonton .NET User Group",
+                    logo: require("../assets/logos/dotNet_logo.png"),
+                    linkTo: "",
+                    slack: "#meetup-dotnet-edmug",
+                },
+                devOps: {
+                    name: "Meetup Dev Ops",
+                    logo: require("../assets/logos/cogs-solid.svg"),
+                    linkTo:
+                        "https://www.meetup.com/startupedmonton/events/pxlrmqyzdbxb/",
+                    slack: "#meetup-devops",
                 },
             },
         };
