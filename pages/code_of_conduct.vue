5ab0e1fa3c5d1beaae065d130743efd74cbd6c0c
Mandy Meindersma <meinders@ualberta.ca>
2021-09-03 13:47:54 -0600

    Actually adding the link to the form

--- pages/code_of_conduct.vue
+++ pages/code_of_conduct.vue
@@ -219,12 +219,14 @@
                     please notify the code of conduct committee by filling out
                     the form below.
                 </p>
-                <button
-                    type="submit"
-                    class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out my-5"
-                >
-                    Report a code of conduct violation
-                </button>
+                <form action="https://forms.gle/NR56fRQUVzdbf1v3A">
+                    <button
+                        type="submit"
+                        class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out my-5"
+                    >
+                        Report a code of conduct violation
+                    </button>
+                </form>
                 <p class="py-2">
                     If you have any questions please email
                     <a
