ddfc71bf580afe9ce8df1082397c732a2269ccea
Derryk Boyd <derrykboyd@gmail.com>
2021-10-30 14:24:25 -0600

    Move home sections into components and add resources section

---
+++ components/MeetupCardSection.vue
@@ -0,0 +1,218 @@
+<template>
+    <div
+        id="meetups"
+        class="relative bg-grey-000 pt-8 -mt-8 pb-16 px-4 sm:px-6 lg:pt-12 lg:px-8"
+    >
+        <div class="absolute inset-0">
+            <div class="h-1/3 sm:h-2/3"></div>
+        </div>
+        <div class="relative max-w-7xl mx-auto">
+            <div class="text-center">
+                <h2
+                    class="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10"
+                >
+                    Meetups We Support
+                </h2>
+            </div>
+            <div
+                class="mt-8 rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px"
+            >
+                <MeetupCard
+                    :name="meetups.ruby.name"
+                    :logo="meetups.ruby.logo"
+                    :link-to="meetups.ruby.linkTo"
+                    :slack="meetups.ruby.slack"
+                >
+                    <span>
+                        YEGRB is Edmonton’s local Ruby Meetup. Ruby is a
+                        dynamic, open source programming language with a focus
+                        on simplicity and productivity. It has an elegant syntax
+                        that is natural to read and easy to write.
+                    </span>
+                </MeetupCard>
+                <MeetupCard
+                    :name="meetups.javascript.name"
+                    :logo="meetups.javascript.logo"
+                    :link-to="meetups.javascript.linkTo"
+                    :slack="meetups.javascript.slack"
+                >
+                    <span>
+                        Exchange.js is an open community of local developers and
+                        designers, who meet monthly to discuss a wide range of
+                        topics within the JavaScript ecosystem.
+                    </span>
+                </MeetupCard>
+                <MeetupCard
+                    :name="meetups.mobileDev.name"
+                    :logo="meetups.mobileDev.logo"
+                    :link-to="meetups.mobileDev.linkTo"
+                    :slack="meetups.mobileDev.slack"
+                    logo-bg="bg-blue-100"
+                >
+                    <span>
+                        Edmonton Mobile Developers is a community of
+                        programmers, designers, and hackers interested in mobile
+                        development. We discuss a broad range of topics and
+                        technologies relevant to the mobile ecosystem while
+                        sharing knowledge and experience.
+                    </span>
+                </MeetupCard>
+                <MeetupCard
+                    :name="meetups.edmontonPy.name"
+                    :logo="meetups.edmontonPy.logo"
+                    :link-to="meetups.edmontonPy.linkTo"
+                    :slack="meetups.edmontonPy.slack"
+                >
+                    <span>
+                        We hope to foster a strong, supportive, and proficient
+                        Python community in the Edmonton area. Everyone
+                        interested in any aspect of Python development is
+                        welcome to join.
+                    </span>
+                </MeetupCard>
+                <MeetupCard
+                    :name="meetups.go.name"
+                    :logo="meetups.go.logo"
+                    :link-to="meetups.go.linkTo"
+                    :slack="meetups.go.slack"
+                >
+                    <span>
+                        Go is an open source programming language that enables
+                        the production of simple, efficient, and reliable
+                        software at scale. Edmonton Go is a meetup to
+                        collaborate, discuss, and learn Go.
+                    </span>
+                </MeetupCard>
+                <MeetupCard
+                    :name="meetups.dotNet.name"
+                    :logo="meetups.dotNet.logo"
+                    :link-to="meetups.dotNet.linkTo"
+                    :slack="meetups.dotNet.slack"
+                >
+                    <span>
+                        The Edmonton .NET User Group is a non-profit
+                        organization which empowers the IT Professionals and
+                        Developers using the Microsoft .NET Framework.
+                    </span>
+                </MeetupCard>
+                <MeetupCard
+                    :name="meetups.devOps.name"
+                    :logo="meetups.devOps.logo"
+                    :link-to="meetups.devOps.linkTo"
+                    :slack="meetups.devOps.slack"
+                    logo-bg="bg-blue-100"
+                >
+                    <span>
+                        DevOps YEG is a platform for DevOps enthusiasts and
+                        professionals to share best practises and knowledge
+                        around managing modern software development operations.
+                    </span>
+                </MeetupCard>
+                <!-- Cog svg icon provided by FontAwesome https://fontawesome.com/license/free -->
+                <MeetupCard
+                    :name="meetups.yegRug.name"
+                    :logo="meetups.yegRug.logo"
+                    :link-to="meetups.yegRug.linkTo"
+                    :slack="meetups.yegRug.slack"
+                    logo-bg="bg-blue-100"
+                >
+                    <span>
+                        The Edmonton R User Group (YEGRUG) is an R-enthusiast
+                        group hosting events on data wrangling to visualization
+                        and web development with R. Learn R, hone your skills,
+                        network, all skill levels are welcome.
+                    </span>
+                </MeetupCard>
+                <MeetupCard
+                    :name="meetups.yegwit.name"
+                    :logo="meetups.yegwit.logo"
+                    :link-to="meetups.yegwit.linkTo"
+                    logo-bg="bg-blue-100"
+                >
+                    <span>
+                        YEGWIT aims to build a strong and safe group for women,
+                        diverse genders, and allies in tech with the goal of
+                        fostering engagement and opportunity, providing
+                        mentorship, and creating community. We are establishing
+                        a group that allows individuals to share ideas,
+                        concerns, thoughts and actions, to create an equitable
+                        environment within the industry.
+                    </span>
+                </MeetupCard>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import MeetupCard from "./MeetupCard.vue";
+
+export default {
+    components: { MeetupCard },
+    data() {
+        return {
+            meetups: {
+                ruby: {
+                    name: "YEGrb",
+                    logo: require("../assets/logos/ruby-240px.png"),
+                    linkTo: "https://yegrb.com/",
+                    slack: "#meetup-ruby",
+                },
+                javascript: {
+                    name: "Exchange.js",
+                    logo: require("../assets/logos/JavaScript-logo-240px.png"),
+                    linkTo: "https://exchangejs.com/",
+                    slack: "#meetup-javascript",
+                },
+                mobileDev: {
+                    name: "Mobile Dev Meetup",
+                    logo: require("../assets/logos/mobile-dev.svg"),
+                    linkTo:
+                        "https://www.meetup.com/startupedmonton/events/knjqbsyccfbgc/",
+                    slack: "#meetup-mobile-dev",
+                },
+                edmontonPy: {
+                    name: "Edmonton.Py",
+                    logo: require("../assets/logos/python-logo.svg"),
+                    linkTo: "http://edmontonpy.com/",
+                    slack: "#meetup-edmontonpy",
+                },
+                go: {
+                    name: "Go Meetup",
+                    logo: require("../assets/logos/Go-Logo_Blue.svg"),
+                    linkTo: "https://edmontongo.org/",
+                    slack: "#meetup-go",
+                },
+                dotNet: {
+                    name: "Edmonton .NET User Group",
+                    logo: require("../assets/logos/dotNet_logo.png"),
+                    linkTo: "http://edmug.net/",
+                    slack: "#meetup-dotnet-edmug",
+                },
+                devOps: {
+                    name: "Meetup Dev Ops",
+                    logo: require("../assets/logos/cogs-solid.svg"),
+                    linkTo:
+                        "https://www.meetup.com/startupedmonton/events/pxlrmqyzdbxb/",
+                    slack: "#meetup-devops",
+                },
+                yegRug: {
+                    name: "Edmonton R User Group",
+                    logo: require("../assets/logos/r-logo.svg"),
+                    linkTo:
+                        "https://www.meetup.com/edmonton-r-user-group-yegrug",
+                    slack: "#meetup-r",
+                },
+                yegwit: {
+                    name: "Edmonton Women In Tech",
+                    logo: require("../assets/logos/yegwit.png"),
+                    linkTo: "https://yegwit.com/",
+                    slack: "#meetup-r",
+                },
+            },
+        };
+    },
+};
+</script>
+
+<style></style>
---
+++ components/ResourcesSection.vue
@@ -0,0 +1,120 @@
+<template>
+    <div
+        id="community-events"
+        class="relative bg-grey-000 pt-8 -mt-8 pb-16 px-4 sm:px-6 lg:pt-12 lg:px-8"
+    >
+        <div class="absolute inset-0">
+            <div class="h-1/3 sm:h-2/3"></div>
+        </div>
+        <div class="relative max-w-7xl mx-auto">
+            <div class="text-center">
+                <h2
+                    class="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10"
+                >
+                    Resources
+                </h2>
+            </div>
+            <div
+                class="mt-8 rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px"
+            >
+                <MeetupCard
+                    :name="events.isaic.name"
+                    :logo="events.isaic.logo"
+                    :link-to="events.isaic.linkTo"
+                >
+                    <span>
+                        ISAIC provides local high-performance computing
+                        infrastructure that is affordable, tailorable and
+                        scalable. Our data sandboxes allow you to experiment and
+                        collaborate easily and securely. Through our
+                        partnerships and supports, ISAIC connects you to applied
+                        knowledge and resources to help move your AI journey
+                        forward. ISAIC is enabling experimentation, scale, and
+                        powering the A.I.mbition of Western Canadian businesses
+                        pursuing AI/ML adoption.
+                    </span>
+                </MeetupCard>
+                <MeetupCard
+                    :name="events.propel.name"
+                    :logo="events.propel.logo"
+                    :link-to="events.propel.linkTo"
+                >
+                    <span>
+                        Ready to boost your startup? Propel is our 12-week
+                        pre-accelerator program designed to support
+                        Alberta-based, early-stage, scalable digital technology
+                        startup founders by offering hands-on, individual
+                        support from expert mentors who have been there, a
+                        roadmap to build sales strategies, investment education
+                        and support as well as programming to find
+                        product-market fit or prepare to scale.
+                    </span>
+                </MeetupCard>
+                <MeetupCard
+                    :name="events.remi.name"
+                    :logo="events.remi.logo"
+                    :link-to="events.remi.linkTo"
+                >
+                    <span>
+                        The REMI program is designed to help startups, SMEs, and
+                        enterprises use AI technology to make better decisions
+                        about how to most effectively reduce greenhouse gases
+                        while also improving operations and increasing revenue.
+                    </span>
+                </MeetupCard>
+                <MeetupCard
+                    :name="events.erin.name"
+                    :link-to="events.erin.linkTo"
+                >
+                    <p>
+                        Any business, whether a freshly-launched start-up or a
+                        midsize firm scaling up its offerings, depends on the
+                        wisdom and counsel of peers to reach the next level –
+                        including high-quality business coaches and mentors. TEC
+                        Edmonton, with support from ERIN, is growing the
+                        coaching capacity in Edmonton.
+                    </p>
+                    <p>
+                        TEC Edmonton (TEC) is seeking visionary business coaches
+                        to help guide promising growth-stage companies to the
+                        next milestones in their development.
+                    </p>
+                </MeetupCard>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import MeetupCard from "./MeetupCard.vue";
+export default {
+    components: { MeetupCard },
+    data() {
+        return {
+            events: {
+                isaic: {
+                    name: "ISAIC",
+                    logo: require("../assets/logos/isaic-icon-logo.png"),
+                    linkTo: "https://isaic.ece.ualberta.ca",
+                },
+                propel: {
+                    name: "Propel at Startup Edmonton",
+                    logo: require("../assets/logos/Startup-Edmonton-Logo-300x116.png"),
+                    linkTo: "https://www.startupedmonton.com/propel",
+                },
+                remi: {
+                    name: "REMI at Amii",
+                    logo: require("../assets/logos/amii.svg"),
+                    linkTo: "https://www.amii.ca/industry-solutions/remi/",
+                },
+                erin: {
+                    name: "Coaching Program from E-RIN",
+                    linkTo: "https://www.edmontonrin.ca/coaching-program",
+                },
+            },
+        };
+    },
+};
+</script>
+
+<style></style>
