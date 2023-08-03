396112514bec6d1f5aa6a3816400b7c464c15c2b
DeeJayJay <deejayjay@zoho.com>
2023-08-02 21:20:50 -0600

    Adds ResourcesSectionSummary component. Adds styls to both EventsSectionSummary and ResourcesSectionSummary

---
+++ pages/all_resources.vue
@@ -0,0 +1,19 @@
+<template>
+    <div
+        class="relative py-8 bg-white overflow-hidden max-w-7xl mx-auto sm:px-4"
+    >
+        <div class="text-gray-500">
+            <ResourcesSection />
+        </div>
+    </div>
+</template>
+
+<script>
+import ResourcesSection from "../components/ResourcesSection.vue";
+
+export default {
+    components: {
+        ResourcesSection,
+    },
+};
+</script>
