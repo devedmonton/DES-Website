b390b34eef9e37ecb411f877402f4f5cebeb18eb
Derryk Boyd <derryk@redmantech.com>
2023-08-28 01:21:04 +0100

    Strings to twMerge not array

--- components/TextAreaStyled.vue
+++ components/TextAreaStyled.vue
@@ -8,10 +8,10 @@ import { twMerge } from "tailwind-merge";
 export default {
     methods: {
         mergeClasses() {
-            return twMerge([
+            return twMerge(
                 "appearance-none block w-full border border-gray-300 rounded-md px-3 py-2 leading-6 transition duration-150 ease-in-out",
                 this.$attrs.class,
-            ]);
+            );
         },
     },
 };
