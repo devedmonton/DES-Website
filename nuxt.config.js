59e62b8f7dd1839fc3ea8d8ad951bb398740a982
Mandy Meindersma <mandy.m.meindersma@gmail.com>
2023-10-19 17:33:59 -0600

    Trying ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxt/image"],
+    ssr: true,
     buildModules: ["@nuxtjs/pwa"],
     alias: {
         pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
