9377f7eae0c612069d2816cae206a575da210621
Marcus Benoit <MarcusBenoit07@gmail.com>
2023-10-31 17:57:21 -0400

    Added color that doesnt blend in with white background

--- components/VButton.vue
+++ components/VButton.vue
@@ -1,17 +1,15 @@
 <template>
     <span class="inline-flex rounded-md shadow-sm">
-        <button
-            type="button"
+        <button type="button"
             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white border-brand-primary focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
             :class="[
                 appearance === 'default'
-                    ? 'text-brand-primary hover:text-white bg-white hover:bg-brand-primary hover:text-white'
+                    ? 'text-brand-primary  bg-white hover:bg-brand-primary hover:text-white'
                     : null,
                 appearance === 'inverted'
-                    ? 'text-white hover:text-brand-primary bg-brand-primary hover:bg-white'
+                    ? 'text-white hover:text-brand-primary hover:bg-[#f8f9fa] hover:border hover:border-blue-200 bg-brand-primary '
                     : null,
-            ]"
-        >
+            ]">
             <slot />
         </button>
     </span>
