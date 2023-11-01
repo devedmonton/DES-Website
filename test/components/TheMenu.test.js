c7436d4e872831c0549e922f7f9380d26d870495
kodyVS <kodyvansloten@gmail.com>
2023-11-01 09:15:52 -0600

    tests updated

--- test/components/TheMenu.test.js
+++ test/components/TheMenu.test.js
@@ -1,8 +1,24 @@
-import { test, expect } from "vitest";
+import { test, expect, vi } from "vitest";
 import { mount } from "@vue/test-utils";
 import TheMenu from "../../components/section/TheMenu.vue";
 
 test("TheMenu.vue", async () => {
     const wrapper = mount(TheMenu);
     expect(wrapper.html()).toBeTruthy();
+});
+
+test("closeMobileMenu", async () => {
+    const wrapper = mount(TheMenu);
+    const spy = vi.spyOn(wrapper.vm, "closeMobileMenu");
+    spy.mockImplementationOnce(() => {});
+    wrapper.vm.closeMobileMenu();
+    expect(spy).toHaveBeenCalled();
+});
+
+test("toggleMobileMenu", async () => {
+    const wrapper = mount(TheMenu);
+    const spy = vi.spyOn(wrapper.vm, "toggleMobileMenu");
+    spy.mockImplementationOnce(() => {});
+    wrapper.vm.toggleMobileMenu();
+    expect(spy).toHaveBeenCalled();
 });
