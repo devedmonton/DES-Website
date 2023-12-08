af81d0830538909ab8fb9dcfa7d2c4dcc39ca863
Mandy Meindersma <mandy.m.meindersma@gmail.com>
2023-12-07 20:53:25 -0700

    Giving meetups their own section and limiting the ones on the home page (#290)

--- components/section/MeetupsSummary.vue
+++ components/section/MeetupsSummary.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="relative pt-8 pb-16 px-4 sm:px-6 lg:pt-12 lg:px-8">
+    <div
+        class="relative bg-grey-000 pt-8 -mt-8 pb-16 px-4 sm:px-6 lg:pt-12 lg:px-8"
+    >
+        <a id="meetups" href="/#meetups" class="anchor"></a>
         <div class="absolute inset-0">
             <div class="h-1/3 sm:h-2/3"></div>
         </div>
@@ -15,7 +18,9 @@
                 class="mt-8 rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px"
             >
                 <ExternalInfoCard
-                    v-for="(meetup, index) in store.getAllMeetups"
+                    v-for="(meetup, index) in store.getLimitedMeetups(
+                        cardLimit,
+                    )"
                     v-bind="meetup"
                     :key="index"
                 >
@@ -23,6 +28,9 @@
                         {{ meetup.meetingTime }}
                     </p>
                 </ExternalInfoCard>
+                <ViewAll :card-limit="cardLimit" link-to="/meetups"
+                    >View All Meetups</ViewAll
+                >
             </div>
         </div>
     </div>
@@ -32,6 +40,13 @@
 import { useMeetupsStore } from "@/store/meetups";
 
 export default {
+    props: {
+        // Use this property to limit the number of cards displayed
+        cardLimit: {
+            type: Number,
+            default: 5,
+        },
+    },
     setup() {
         const store = useMeetupsStore();
         return { store };
