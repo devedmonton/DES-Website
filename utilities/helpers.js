9bcab2fb797ec513cefc18da0619620efa2e45e2
DeeJayJay <deejayjay@zoho.com>
2024-01-22 17:41:13 -0700

    Improves the shuffle logic.

--- utilities/helpers.js
+++ utilities/helpers.js
@@ -9,9 +9,15 @@ export const shuffleArray = (array) => {
     ) {
         const randomIndex = Math.floor(Math.random() * (currentIndex + 1));
 
+        // If current index is the same as random index, run the loop again for the same index
+        if (currentIndex === randomIndex) {
+            currentIndex++;
+            continue;
+        }
+
         [shuffledArray[currentIndex], shuffledArray[randomIndex]] = [
             shuffledArray[randomIndex],
-            shuffledArray[currentIndex],
+            shuffledArray[currentIndex]
         ];
     }
 
