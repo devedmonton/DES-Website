63a1a04c1fa137c04a58f3b338c1bd130d36188d
Arash <arashi.sheyda@gmail.com>
2024-03-23 09:22:45 -0600

    chore: update slack channels

--- constants/slack.ts
+++ constants/slack.ts
@@ -3,26 +3,26 @@ export const SLACK_URL = 'https://join.slack.com/t/devedmonton/shared_invite/zt-
 export const channels = [
   {
     name: 'introductions',
-    description: 'to say Hi',
+    description: 'to say Hi.',
   },
   {
     name: 'opportunities',
-    description: 'where people can post and look for jobs, internships, volunteer positions or more',
+    description: 'where people can post and look for jobs, internships, volunteer positions or more.',
   },
   {
     name: 'events',
-    description: 'specific channels listed in the home page languages or tech stacks you\'re interested in',
-  },
-  {
-    name: 'random',
-    description: 'for casual conversations',
+    description: 'to showcase local events and meetups about tech.',
   },
   {
     name: 'dev-talk',
-    description: 'for more tech focused conversations',
+    description: 'for more tech focused conversations.',
   },
   {
     name: 'dev-help',
-    description: 'for questions about anything tech that you are stuck on or curious about',
+    description: 'for questions about anything tech that you are stuck on or curious about.',
+  },
+  {
+    name: 'random',
+    description: 'for casual conversations.',
   },
 ]
