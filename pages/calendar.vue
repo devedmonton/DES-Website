953fd078285dd951c2e3801bb76ca358d205ea40
Keifer Erikson <keifer@erikson.dev>
2024-05-24 15:53:47 -0600

    WIP: switching to useLazyAsyncData instead of onMounted

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 const group = ref({ name: 'Calendar', items: [] })
 
+const { pending, data: events } = await useLazyAsyncData('events', fetchEvents)
+
+watch(events, (newEvents) => {
+  group.value = { name: 'Calendar', items: newEvents }
+})
+
 const title = 'Calendar'
 const description = 'List of all upcoming events in Edmonton'
 
@@ -13,20 +19,16 @@ defineOgImage({
   component: 'AppOgImageFrame',
   icon: 'i-ph-calendar-duotone',
 })
-
-onMounted(async () => {
-  try {
-    const items = await fetchEvents()
-    group.value = { name: 'Calendar', items }
-  }
-  catch (error) {
-    console.error('Failed to fetch events:', error)
-  }
-})
 </script>
 
 <template>
   <main>
-    <AppCalendar :group="group" />
+    <AppCalendar
+      v-if="!pending"
+      :group="group"
+    />
+    <div v-if="pending">
+      Loading...
+    </div>
   </main>
 </template>
