c9f8a777bd31f71f7eeb4aa833c879b33dcd8597
Yaroslav Horetskyi <yhoretskyi@canceriq.com>
2024-05-29 12:39:28 -0600

    feat: create colors with opacity

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,8 @@ export default <Partial<Config>>{
       colors: {
         primary: '#265DAD',
         secondary: '#37C5F0',
+        primaryWithOpacity: 'rgba(38,93,173,0.5)',
+        secondaryWithOpacity: 'rgba(55,197,240,0.5)',
       },
     },
   },
