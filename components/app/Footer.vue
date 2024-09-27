8686c395f18a51ab6e44d9edad729890ff6be7fb
AACHAL2303PATIL <aachalptil2307@gmail.com>
2024-09-27 22:12:39 +0530

    feat:rearranging header

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -7,10 +7,7 @@ const { data: navigation } = await useAsyncData('navigation:footer', () => fetch
 </script>
 
 <template>
-  <footer
-    id="footer"
-    class="container mx-auto"
-  >
+  <footer id="footer" class="container mx-auto">
     <div class="w-full max-w-screen-xl mx-auto border-t border-neutral-400/20">
       <div class="flex md:flex-row flex-col gap-8 items-center justify-between py-4">
         <NuxtLink
@@ -18,17 +15,10 @@ const { data: navigation } = await useAsyncData('navigation:footer', () => fetch
           to="/"
           class="flex justify-center md:order-1 order-2"
         >
-          <AppLogo
-            :scrolling="true"
-            class="h-12"
-          />
+          <AppLogo :scrolling="true" class="h-12" />
         </NuxtLink>
         <ul class="flex flex-wrap justify-center gap-4 items-center text-sm font-medium text-gray-600 dark:text-gray-400 md:mx-0 mx-2 order-1">
-          <li
-            v-for="item of navigation"
-            :key="item.title"
-            class="mb-4"
-          >
+          <li v-for="item of navigation" :key="item.title" class="mb-4">
             <NuxtLink
               :title="item.title"
               :to="item._path"
@@ -37,6 +27,9 @@ const { data: navigation } = await useAsyncData('navigation:footer', () => fetch
             >
               {{ item.title }}
             </NuxtLink>
+          </li>
+          <li class="mb-4">
+            <NuxtLink to="/contact" exact-active-class="text-primary dark:text-secondary bg-gray-400/20" class="hover:underline py-1.5 px-4 rounded-lg">Contact</NuxtLink>
           </li>
         </ul>
       </div>
