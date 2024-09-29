c332f31513ee19d44585d676232baecca80c18ff
AACHAL2303PATIL <aachalpatil2307@gmail.com>
2024-09-29 18:32:07 +0530

    Rearranging-the-Header

--- constants/navigation.ts
+++ constants/navigation.ts
@@ -22,8 +22,8 @@ export const navigation: NavItem[] = [
     to: '/resources',
   },
   {
-    name: 'Companies', // Changed from 'Contact' to 'Companies'
-    icon: 'i-ph-building', // Choose an appropriate icon for companies
-    to: '/companies', // Link to the new companies page
+    name: 'Companies', 
+    icon: 'i-ph-building',
+    to: '/companies',
   },
 ]
