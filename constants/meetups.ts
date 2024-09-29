312156a62179483f45d86c1801abc8aaf01e43fd
Anubhav2k3 <anubhavp2k3@gmail.com>
2024-09-29 09:38:17 +0530

    Leetcode Night

--- constants/meetups.ts
+++ constants/meetups.ts
@@ -2,12 +2,10 @@ export const meetups = [
   {
     name: 'Leetcode Night: Code, Collaborate, Conquer!',
     description: `Join us for Leetcode Night, where coders of all levels tackle Leetcode problems together. Whether you're prepping for interviews or just want to sharpen your skills, this is the perfect chance to collaborate, share strategies, and learn new techniques. Bring your laptop, team up, and let's solve some coding challenges! Any meetups will be posted on the Edmonton Unlimited meetup.com
-Ready to level up? See you at Leetcode Night!A combination of the YEGRB, Exchange.js, and Edmonton.py communities!`,
-    to: `https://www.meetup.com/edmontonunlimited/events/`,
+Ready to level up? See you at Leetcode Night!A combination of the YEGRB, Exchange.js, and Edmonton.py communities!Join our Discord! Connect via #yegleet and #yegleetevents!`,
+    to: `https://discord.gg/hDGT9F9hbp`,
     image: '/meetups/leetcode.png',
-    socials: {
-      slack: '#meetup-Leetcode Night',
-    },
+    
   },
   {
     name: 'Javascript, Ruby and Python Meetup',
