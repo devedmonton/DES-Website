57c49b7ed3d67dd944ee27a03d32339308be1b6d
Anjali Jha <109814198+blizet@users.noreply.github.com>
2024-10-07 06:01:47 +0530

    (Fix #379) Addes Bsides Edmonton to meetups (#391)
    * Update meetups.ts
    
    * merge conflict
    
    ---------
    
    Co-authored-by: Mandy Meindersma <mandy.m.meindersma@gmail.com>

--- constants/meetups.ts
+++ constants/meetups.ts
@@ -62,6 +62,12 @@ Ready to level up? See you at Leetcode Night!A combination of the YEGRB, Exchang
     description: 'We meet on a monthly-ish basis to discuss security, drink beer, and socialize. If you\'re into that sort of thing then you should probably join us. If you hate waiting, and who  can blame you, you\'re more than welcome to join us in Slack. Tweet @yegsec for an invite!',
   },
   {
+    name: 'Bsides Edmonton',
+    image: 'events/bsides.png',
+    to: 'https://www.bsidesedmonton.org/',
+    description: 'BSides Edmonton is an annual Cyber Security Conference in the City of Edmonton.Security BSides is a community-driven Cyber Security Conference by cybersecurity community members. The goal is to expand the spectrum of conversation beyond the traditional confines of space and time. It creates opportunities for individuals to both present and participate in an intimate atmosphere that encourages collaboration.',
+  },
+  {
     name: 'Demo Camp',
     image: '/events/DemoCamp.png',
     to: 'https://www.eventbrite.ca/o/democamp-edmonton-56126644423',
