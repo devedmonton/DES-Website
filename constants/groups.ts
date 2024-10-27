c4476d13b153e0d7bb33275ceb56fb84ca1cc0c8
Atremblay9 <atremblay9@nait.ca>
2024-10-26 20:47:51 -0600

    Update issues

---
+++ constants/groups.ts
@@ -0,0 +1,60 @@
+export const groups = [
+  {
+    name: 'Product Edmonton',
+    to: `http://productedmonton.slack.com/`,
+    image: '/meetups/slack.png',
+    description: 'We are a group dedicated to collectively increasing the skill-level and connectedness of the entire Edmonton Product Community.',
+  },
+  {
+    name: 'Startup Edmonton',
+    to: `https://yegstartup.slack.com/join/shared_invite/zt-czl41ymv-IlCrO1Na2KwqV9pb37v4kQ#/shared-invite/email`,
+    image: '/meetups/slack.png',
+  },
+  {
+    name: 'Digital Edmonton (UX/IA)',
+    to: 'https://join.slack.com/t/digitaledmonton/shared_invite/zt-7ka8gm45-cVNFyy1wp9R0wunz46vrlQ',
+    image: '/meetups/slack.png',
+  },
+  {
+    name: 'YEG DEV',
+    to: 'https://yegdev.slack.com/',
+    image: '/meetups/slack.png',
+  },
+  {
+    name: 'Google Developer Group',
+    to: 'http://gdg-cloud-edmonton.slack.com/',
+    image: '/meetups/slack.png',
+  },
+  {
+    name: 'Edmonton Data science',
+    to: 'https://discord.gg/uVuTx8G3',
+    image: '/meetups/discord.png',
+  },
+  {
+    name: 'Alberta programmers',
+    to: 'https://discord.gg/XEjjP7KY',
+    image: '/meetups/discord.png',
+  },
+  {
+    name: 'Alberta Game Developers',
+    to: 'https://discord.gg/7ZqjBCH4',
+    image: '/meetups/discord.png',
+  },
+  {
+    name: 'Junior Developer',
+    to: 'https://discord.gg/qK9ydrA2SC',
+    image: '/meetups/discord.png',
+  },
+  {
+    name: 'UX Edmonton',
+    to: 'https://www.facebook.com/groups/59071219138',
+    image: '/meetups/facebook.png',
+    description: 'UX YEG is dedicated to connecting Edmontons vibrant user experience community. Our goal is to offer the opportunity for likeminded professionals to connect, share ideas and learn new skills.',
+  },
+  {
+    name: 'YEG Women in Tech',
+    to: 'https://www.facebook.com/groups/YEGWomenInTech',
+    image: '/meetups/facebook.png',
+  },
+
+]
