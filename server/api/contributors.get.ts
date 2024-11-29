7d2dae54a026725415e0dba15f22a294bf9f8017
Daniel Mouris <dgmouris@gmail.com>
2024-11-29 14:59:11 -0700

    added type script

--- server/api/contributors.get.ts
+++ server/api/contributors.get.ts
@@ -1,10 +1,14 @@
+import type {
+  GitHubUser,
+  RESTApiResponse,
+} from '~/types'
+
 /**
  * API endpoint for fetching contributors.
  * @example
  * // Example usage:
  * // GET /api/contributors
  */
-// eslint-disable-next-line
 export default defineEventHandler(async (event) => {
   // we're going to use our key
   const { githubAppAPIKey } = useRuntimeConfig(event).githubAPI
@@ -15,24 +19,24 @@ export default defineEventHandler(async (event) => {
   // make the request
   try {
     // await the collaborators endpoint from github using nitros fetch
-    const data = await $fetch(CONTRIBUTORS_ENDPOINT, {
+    const data: GitHubUser[] = await $fetch(CONTRIBUTORS_ENDPOINT, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${githubAppAPIKey}`,
       },
     })
-    return {
+    const response = <RESTApiResponse<GitHubUser[]>>{
       statusCode: 200,
-      message: `Contributors fetch successfully`,
-      contributors: data,
+      message: 'Contributors fetch successfully',
+      data,
     }
+    return response
   }
-  catch (error) {
+  catch {
     throw createError({
       statusCode: 400,
       statusMessage: 'Bad Request',
       message: 'Error while fetching contributors.',
-      error: 'Error while fetching contributors.',
     })
   }
 })
