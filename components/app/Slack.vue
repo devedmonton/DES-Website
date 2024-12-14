69a67e2a72759999e661532f2cbbf123a9204f31
Arash Ari Sheyda <arashi.sheyda@gmail.com>
2024-12-14 09:45:19 -0700

    update responsiveness & custom speed

--- components/app/Slack.vue
+++ components/app/Slack.vue
@@ -4,18 +4,22 @@ const selected = ref(channels[0])
 const circles = [
   {
     distance: '3.5cqw',
-    items: (['i-logos-c-plusplus', 'i-skill-icons-rust', 'i-logos-c', 'i-logos-git-icon']),
+    speed: 11,
+    items: (['i-logos-c-plusplus', 'i-skill-icons-rust', 'i-logos-c', 'i-logos-git-icon', 'i-logos-nginx']),
   },
   {
     distance: '7cqw',
+    speed: 9,
     items: (['i-logos-django-icon', 'i-unjs-nitro', 'i-logos-nodejs-icon', 'i-logos-nestjs', 'i-logos-spring-icon', 'i-logos-laravel', 'i-logos-dotnet']),
   },
   {
     distance: '10.5cqw',
+    speed: 11,
     items: (['i-logos-javascript', 'i-logos-ruby', 'i-logos-python', 'i-logos-go', 'i-logos-java', 'i-logos-typescript-icon', 'i-logos-swift', 'i-logos-dart']),
   },
   {
     distance: '14.5cqw',
+    speed: 10,
     items: (['i-logos-vitejs', 'i-logos-vue', 'i-logos-react', 'i-logos-nuxt-icon', 'i-logos-svelte-icon', 'i-logos-nextjs-icon', 'i-logos-angular-icon', 'i-logos-ionic-icon', 'i-logos-tailwindcss-icon', 'i-logos-vitest', 'i-logos-playwright']),
   },
 ]
@@ -31,17 +35,17 @@ const circles = [
     >
       <div class="flex flex-col gap-4 justify-between text-center items-center p-4">
         <Icon
-          class="w-10 h-10 mb-4"
+          class="lg:w-10 lg:h-10 h-5 w-5 mb-4"
           name="i-logos-slack-icon"
         />
         <div class="flex flex-col gap-2 items-start">
-          <span class="text-white font-bold text-lg">
+          <span class="text-white font-bold md:text-lg text-xs">
             Channels to Checkout
           </span>
           <button
             v-for="channel of channels"
             :key="channel"
-            class="px-2.5 py-1 rounded-full text-white truncate text-xs hover:bg-[#7F4484] w-full text-start"
+            class="px-2.5 py-1 rounded-full text-white truncate text-xs hover:bg-[#7F4484] w-full "
             :class="{ 'bg-[#7F4484]': channel.name === selected.name }"
             @click="selected = channel"
           >
@@ -49,16 +53,16 @@ const circles = [
           </button>
         </div>
         <NuxtImg
-          class="rounded-lg w-10 h-10 object-cover border"
+          class="rounded-lg lg:w-10 lg:h-10 h-5 w-5 object-cover border"
           src="/assets/images/avatar.png"
           alt="avatar"
           loading="lazy"
         />
       </div>
-      <div class="relative flex flex-col gap-4 bg-white dark:bg-black rounded-lg justify-between">
+      <div class="relative flex flex-col gap-4 bg-white dark:bg-black rounded-lg justify-between overflow-auto">
         <div>
-          <div class="p-4 border-b border-neutral-400/40 flex justify-between items-center">
-            <div class="">
+          <div class="p-4 border-b border-neutral-400/40 flex justify-between items-center gap-2">
+            <div class="whitespace-nowrap">
               <span class="font-bold"># {{ selected.name }}</span>
             </div>
             <div class="flex gap-2">
@@ -88,7 +92,7 @@ const circles = [
           </div>
         </div>
 
-        <div class="mt-[-5%] grid h-full w-full grid-cols-1 place-items-center [&>*]:[grid-area:1/1]">
+        <div class="mt-[-5%] sm:grid hidden h-full w-full grid-cols-1 place-items-center [&>*]:[grid-area:1/1]">
           <div class="grid h-full w-full grid-cols-1 place-items-center [&>*]:[grid-area:1/1] slack-orbit-bg">
             <div class="slack-orbit-circle w-[7cqw]" />
             <div class="slack-orbit-circle w-[14cqw]" />
@@ -103,8 +107,9 @@ const circles = [
                 :key="item"
                 class="absolute xl:w-6 xl:h-6 lg:w-5 lg:h-5 md:w-4 md:h-4 sm:w-3 sm:h-3 w-2 h-2 slack-orbit"
                 :style="{
-                  '--distance': circle.distance,
-                  '--delay': `${(idx / circle.items.length) * -10}s`,
+                  '--orbit-distance': circle.distance,
+                  '--orbit-speed': `${circle.speed}s`,
+                  '--orbit-delay': `${(idx / circle.items.length) * -circle.speed}s`,
                 }"
                 :name="item"
               />
@@ -127,16 +132,16 @@ const circles = [
 }
 
 .slack-orbit {
-    animation: orbit 10s linear var(--delay, -3s) infinite;
+    animation: orbit var(--orbit-speed, 10s) linear var(--orbit-delay, -3s) infinite;
     animation-play-state: running;
 }
 
 @keyframes orbit {
   0% {
-    transform: rotate(0deg) translateY(var(--distance)) rotate(0deg);
+    transform: rotate(0deg) translateY(var(--orbit-distance)) rotate(0deg);
   }
   100% {
-    transform: rotate(1turn) translateY(var(--distance)) rotate(-1turn);
+    transform: rotate(1turn) translateY(var(--orbit-distance)) rotate(-1turn);
   }
 }
 
