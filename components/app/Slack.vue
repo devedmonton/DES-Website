242d8f8194fe4d4e76c1e5e9bc63b3627ee0a9fa
Arash Ari Sheyda <38922203+arashsheyda@users.noreply.github.com>
2024-12-15 19:20:19 -0700

    refactor: slack orbit icons design (#447)
    * refactor: slack orbit icons design
    
    * chore: small fixes
    
    * chore: border in dark mode
    
    * update
    
    * update responsiveness & custom speed
    
    ---------
    
    Co-authored-by: Mandy Meindersma <mandy.m.meindersma@gmail.com>

--- components/app/Slack.vue
+++ components/app/Slack.vue
@@ -1,5 +1,28 @@
 <script setup>
 const selected = ref(channels[0])
+
+const circles = [
+  {
+    distance: '3.5cqw',
+    speed: 11,
+    items: (['i-logos-c-plusplus', 'i-skill-icons-rust', 'i-logos-c', 'i-logos-git-icon', 'i-logos-nginx']),
+  },
+  {
+    distance: '7cqw',
+    speed: 9,
+    items: (['i-logos-django-icon', 'i-unjs-nitro', 'i-logos-nodejs-icon', 'i-logos-nestjs', 'i-logos-spring-icon', 'i-logos-laravel', 'i-logos-dotnet']),
+  },
+  {
+    distance: '10.5cqw',
+    speed: 11,
+    items: (['i-logos-javascript', 'i-logos-ruby', 'i-logos-python', 'i-logos-go', 'i-logos-java', 'i-logos-typescript-icon', 'i-logos-swift', 'i-logos-dart']),
+  },
+  {
+    distance: '14.5cqw',
+    speed: 10,
+    items: (['i-logos-vitejs', 'i-logos-vue', 'i-logos-react', 'i-logos-nuxt-icon', 'i-logos-svelte-icon', 'i-logos-nextjs-icon', 'i-logos-angular-icon', 'i-logos-ionic-icon', 'i-logos-tailwindcss-icon', 'i-logos-vitest', 'i-logos-playwright']),
+  },
+]
 </script>
 
 <template>
@@ -10,19 +33,19 @@ const selected = ref(channels[0])
     <div
       class="relative z-10 grid grid-cols-[100px_1fr] gap-4 bg-gradient-to-t from-[#5E2362] to-[#78327D] p-4 rounded-xl h-full"
     >
-      <div class="flex flex-col gap-4 justify-center text-center items-center p-4">
+      <div class="flex flex-col gap-4 justify-between text-center items-center p-4">
         <Icon
-          class="w-10 h-10 mb-4"
+          class="lg:w-10 lg:h-10 h-5 w-5 mb-4"
           name="i-logos-slack-icon"
         />
         <div class="flex flex-col gap-2 items-start">
-          <span class="text-white font-bold text-lg">
+          <span class="text-white font-bold md:text-lg text-xs">
             Channels to Checkout
           </span>
           <button
             v-for="channel of channels"
             :key="channel"
-            class="px-2.5 py-1 rounded-full text-white truncate text-xs hover:bg-[#7F4484] w-full text-start"
+            class="px-2.5 py-1 rounded-full text-white truncate text-xs hover:bg-[#7F4484] w-full "
             :class="{ 'bg-[#7F4484]': channel.name === selected.name }"
             @click="selected = channel"
           >
@@ -30,155 +53,72 @@ const selected = ref(channels[0])
           </button>
         </div>
         <NuxtImg
-          class="rounded-lg w-10 h-10 object-cover border"
+          class="rounded-lg lg:w-10 lg:h-10 h-5 w-5 object-cover border"
           src="/assets/images/avatar.png"
           alt="avatar"
           loading="lazy"
         />
       </div>
-      <div class="relative flex flex-col gap-4 bg-white dark:bg-black p-8 rounded-lg">
-        <div class="border-b border-neutral-400/40 pb-2">
-          <span class="font-bold"># {{ selected.name }}</span>
-          <span class="text-sm font-light ml-1">
+      <div class="relative flex flex-col gap-4 bg-white dark:bg-black rounded-lg justify-between overflow-auto">
+        <div>
+          <div class="p-4 border-b border-neutral-400/40 flex justify-between items-center gap-2">
+            <div class="whitespace-nowrap">
+              <span class="font-bold"># {{ selected.name }}</span>
+            </div>
+            <div class="flex gap-2">
+              <!-- only show ColorMode in client side because of hydration mismatch -->
+              <ClientOnly>
+                <AppColorMode class="border border-neutral-400/40 text-neutral-400 px-2 py-1 !text-sm rounded" />
+              </ClientOnly>
+              <!-- TODO: go to contributors page -->
+              <div class="flex gap-1 items-center border border-neutral-400/40 text-neutral-400 px-2 py-1 rounded">
+                <Icon
+                  class="block text-lg"
+                  name="i-ph-users-duotone"
+                />
+                <!-- TODO: show contributors actual number, dynamically -->
+                <span class="text-xs">55</span>
+              </div>
+              <div class="border border-neutral-400/40 text-neutral-400 px-2 py-1 rounded">
+                <Icon
+                  class="block text-lg"
+                  name="i-ph-headphones-duotone"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="p-4">
             {{ selected.description }}
-          </span>
+          </div>
         </div>
-        <div class="relative flex items-center justify-center h-full">
-          <div class="relative md:flex hidden justify-center">
-            <!-- bottom -->
-            <div class="">
-              <div
-                class="relative h-[30rem] w-[30rem] rounded-full rotating-2 border-x border-[#FDCDCD]"
-                style="background: radial-gradient(50% 50% at 50% 50%, #FDCDCD 0%, var(--color-bg-dark) 80%, #FDCDCD15);"
-              >
-                <Icon
-                  class="absolute top-10 right-20 w-6 h-6"
-                  name="i-logos-javascript"
-                />
 
-                <Icon
-                  class="absolute top-6 left-24 w-6 h-6"
-                  name="i-logos-ruby"
-                />
-
-                <Icon
-                  class="absolute bottom-10 left-20 w-6 h-6"
-                  name="i-logos-typescript-icon"
-                />
-
-                <Icon
-                  class="absolute bottom-10 right-20 w-6 h-6"
-                  name="i-logos-python"
-                />
-
-                <Icon
-                  class="absolute -bottom-2 left-56 w-6 h-6"
-                  name="i-logos-java"
-                />
-
-                <Icon
-                  class="absolute top-56 -left-3 w-6 h-6"
-                  name="i-logos-swift"
-                />
-
-                <Icon
-                  class="absolute top-56 -right-3 w-6 h-6"
-                  name="i-logos-c-plusplus"
-                />
-              </div>
-            </div>
-
-            <!-- middle -->
-            <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-              <div
-                class="relative h-[22rem] w-[22rem] rounded-full rotating-3 border-x border-[#37C5F0]"
-                style="background: radial-gradient(50% 50% at 50% 50%, #37C5F0 0%, var(--color-bg-dark) 85%);"
-              >
-                <Icon
-                  class="absolute bottom-10 left-10 w-6 h-6 rounded"
-                  name="i-logos-nodejs-icon-alt"
-                />
-
-                <Icon
-                  class="absolute bottom-8 right-10 w-7 h-7 rounded"
-                  name="i-logos-laravel"
-                />
-
-                <Icon
-                  class="absolute top-10 left-10 w-6 h-6 rounded"
-                  name="i-logos-django-icon"
-                />
-
-                <Icon
-                  class="absolute top-10 right-10 w-6 h-6 rounded"
-                  name="i-logos-spring-icon"
-                />
-              </div>
-            </div>
-
-            <!-- top -->
-            <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-              <div
-                class="relative h-[15rem] w-[15rem] rounded-full rotating border-x border-[#00DC82]"
-                style="background: radial-gradient(50% 50% at 50% 50%, #00DC82 0%, var(--color-bg-dark) 85%);"
-              >
-                <Icon
-                  class="absolute bottom-10 right-4 w-6 h-6"
-                  name="i-logos-nuxt-icon"
-                />
-
-                <Icon
-                  class="absolute -bottom-3 right-1/2 w-6 h-6"
-                  name="i-logos-vue"
-                />
-
-                <Icon
-                  class="absolute bottom-20 -left-2 w-6 h-6"
-                  name="i-logos-svelte-icon"
-                />
-
-                <Icon
-                  class="absolute top-20 -right-2 w-6 h-6"
-                  name="i-logos-angular-icon"
-                />
-
-                <Icon
-                  class="absolute -top-1.5 left-36 w-6 h-6"
-                  name="i-logos-react"
-                />
-
-                <Icon
-                  class="absolute top-10 left-2 w-6 h-6"
-                  name="i-logos-vitejs"
-                />
-              </div>
-            </div>
-
-            <div
-              class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-black p-4 rounded-full shadow-xl"
-            >
-              <AppLogo
-                :scrolling="true"
-                class="w-14 h-14"
+        <div class="mt-[-5%] sm:grid hidden h-full w-full grid-cols-1 place-items-center [&>*]:[grid-area:1/1]">
+          <div class="grid h-full w-full grid-cols-1 place-items-center [&>*]:[grid-area:1/1] slack-orbit-bg">
+            <div class="slack-orbit-circle w-[7cqw]" />
+            <div class="slack-orbit-circle w-[14cqw]" />
+            <div class="slack-orbit-circle w-[21cqw]" />
+            <div class="slack-orbit-circle w-[29cqw]" />
+            <div class="slack-orbit-circle w-[36cqw]" />
+          </div>
+          <div class="relative z-10 grid h-full w-full grid-cols-1 place-items-center">
+            <template v-for="circle in circles">
+              <Icon
+                v-for="item, idx in circle.items"
+                :key="item"
+                class="absolute xl:w-6 xl:h-6 lg:w-5 lg:h-5 md:w-4 md:h-4 sm:w-3 sm:h-3 w-2 h-2 slack-orbit"
+                :style="{
+                  '--orbit-distance': circle.distance,
+                  '--orbit-speed': `${circle.speed}s`,
+                  '--orbit-delay': `${(idx / circle.items.length) * -circle.speed}s`,
+                }"
+                :name="item"
               />
-            </div>
+            </template>
           </div>
           <AppLogo
             :scrolling="true"
-            class="md:hidden w-40 h-40"
+            class="xl:w-16 xl:h-16 lg:w-12 lg:h-12 md:w-10 md:h-10 sm:w-8 sm:h-8 w-5 h-5"
           />
-        </div>
-        <div class="absolute bottom-4">
-          <AppButton
-            :to="SLACK_URL"
-            class="bg-primary text-white px-4 py-1 rounded-lg flex items-center gap-2 hover:text-primary hover:border-primary"
-          >
-            <Icon
-              class="w-4 h-4"
-              name="i-simple-icons-slack"
-            />
-            Join Slack
-          </AppButton>
         </div>
       </div>
     </div>
@@ -186,33 +126,27 @@ const selected = ref(channels[0])
 </template>
 
 <style>
-:root {
-  --color-bg-dark: white;
+.slack-orbit-bg {
+  mask-image: radial-gradient(70% 50% at center,#000 var(--fade-start,25%),transparent 100%);
+  -webkit-mask-image: radial-gradient(70% 50% at center,#000 var(--fade-start,25%),transparent 100%);
 }
 
-.dark {
-  --color-bg-dark: #000;
+.slack-orbit {
+    animation: orbit var(--orbit-speed, 10s) linear var(--orbit-delay, -3s) infinite;
+    animation-play-state: running;
 }
 
-.rotating {
-  animation: rotating 7s linear infinite
-}
-
-.rotating-2 {
-  animation: rotating 25s linear infinite
-}
-
-.rotating-3 {
-  animation: rotating 13s linear infinite
-}
-
-@keyframes rotating {
+@keyframes orbit {
   0% {
-    transform: rotate(0)
+    transform: rotate(0deg) translateY(var(--orbit-distance)) rotate(0deg);
   }
+  100% {
+    transform: rotate(1turn) translateY(var(--orbit-distance)) rotate(-1turn);
+  }
+}
 
-  to {
-    transform: rotate(1turn)
-  }
+.slack-orbit-circle {
+  @apply border border-neutral-400/40 aspect-square rounded-[9999px] transition-all duration-300;
+  @apply shadow-[rgba(0,0,0,0.2)_0px_0px_10px_0px] dark:shadow-[rgba(255,255,255,0.3)_0px_0px_30px_0px];
 }
 </style>
