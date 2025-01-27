c0f698a691d42370a78b46b16e33e6fb774589ec
Daniel Mouris <dgmouris@gmail.com>
2025-01-27 09:02:40 -0700

    Added wordpress meetup

--- server/api/import_events.js
+++ server/api/import_events.js
@@ -32,6 +32,7 @@ const ICAL_URLS = [
   'http://api.lu.ma/ics/get?entity=calendar&id=cal-N0bfLPnGQ1LC86P', // Leetnight
   'https://www.meetup.com/Edmonton-NET-User-Group/events/ical/', // .NET user group
   'https://www.meetup.com/flutter-edmonton/events/ical/', // flutter edmonton
+  'https://www.meetup.com/edmonton-wordpress-meetup-group/events/ical/', // wordpress meetup
 ]
 
 // gets the ICAL from the urls above.
